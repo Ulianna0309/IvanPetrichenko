3759168631b65aef3c298107b5892d469a81da66
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2021-11-14 20:30:53 +0200

    for

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?');
+/*const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?');
 const personalMovieDB = {
     count:  numberOfFilms,
     movies: {},
@@ -18,8 +18,42 @@ personalMovieDB.movies[c] = d;
 
 
 
-
-
-
-
 console.log(personalMovieDB);
+
+*/
+const num = 50;
+switch(num){
+    case 50:
+        console.log('Оно');
+        break;
+    case 100:
+        console.log('Не верно');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
+
+
+let num2 = 50;
+
+/*while(num2 <= 55){
+    console.log(num2);
+    num2++;
+}
+
+do{
+    console.log(num2);
+    num2++;
+}
+
+while(num2 < 55);*/
+
+for (let i = 1; i < 10; i++){
+
+    if(i === 6){
+        //break;
+        continue;
+    }
+    console.log(i);
+}
