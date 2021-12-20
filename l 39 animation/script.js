a51afe718a6e93f0d2e903e77fffe762301bf9ef
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2021-12-20 22:48:12 +0200

    date

--- l 39 animation/script.js
+++ l 39 animation/script.js
@@ -33,3 +33,32 @@ let id = setTimeout(function log(){
     console.log('Hello');
     id = setTimeout(log, 500);
 }, 500);*/
+
+
+//Дата
+const now = new Date();
+console.log(now);
+
+const now2 = new Date();
+console.log(now2.getFullYear());
+console.log(now2.getMonth());
+console.log(now2.getDate());
+console.log(now2.getDay());
+console.log(now2.getUTCHours());
+
+console.log(now2.getTimezoneOffset());
+console.log(now2.getTime());
+console.log(now2.setHours(18, 40));
+console.log(now2);
+console.log(now2.setHours(40));
+console.log(now2);
+
+let start = new Date();
+for(let i = 0; i < 1000000; i++){
+    let some = i**3;
+}
+let end = new Date();
+alert(`Цикл отработал зa ${end - start} милисекунд`);
+
+
+
