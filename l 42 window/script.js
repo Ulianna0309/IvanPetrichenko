04b59d9410931a370880297795a0275b99eb51cd
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2021-12-27 19:54:35 +0200

    l 45 this

--- l 42 window/script.js
+++ l 42 window/script.js
@@ -27,3 +27,31 @@ console.log(style.display);
 
 console.log(document.documentElement.clientWidth);
 console.log(document.documentElement.scrollTop);
+
+
+//Функции-конструкторы
+
+function User(name, id){
+   this.name = name;
+   this.id = id;
+   this.human = true;
+   this.hello = function(){
+      console.log(`Hello ${this.name}`);
+   };
+}
+
+User.prototype.exit = function(name){
+   console.log(`Пользователь ${this.name} ушел`)
+};
+
+const ivan = new User('Ivan', 35);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+
+console.log(ivan);
+console.log(alex);
