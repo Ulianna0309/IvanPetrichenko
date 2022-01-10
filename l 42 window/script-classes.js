529f75404895e9b563c5a4f11f40361afe257c4d
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-01-10 21:44:53 +0200

    f 49 l

--- l 42 window/script-classes.js
+++ l 42 window/script-classes.js
@@ -31,3 +31,14 @@ const long = new  Rectangle(20, 100);
 
 console.log(square.calcArea());
 console.log(long.calcArea());*/
+
+
+const log = function(a, b, ...rest){
+   console.log(a, b, rest);
+}
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2){
+   console.log(number * basis)
+}
+calcOrDouble(3);
