9636dc66d4b32d89eb13e16db062399646fcd8d9
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-14 13:57:10 +0200

    jquery

--- jquery/script.js
+++ jquery/script.js
@@ -1,4 +1,19 @@
 import * as $ from 'jquery';
 
-$('#btn');
-console.log(btn);
+$(document).ready(function(){
+    $('.list-item:first').hover(function(){
+      $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(2)').on('click', function(){
+      $('.image:even').fadeToggle('slow');
+    });
+
+
+    $('.list-item:eq(4)').on('click', function(){
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 2000);
+    });
+});
